c2260f94b7c47f3f6e7cadfdd28d120e7b467414
Paulo Freitas <paulinho.f533@gmail.com>
2021-10-20 21:42:36 -0300

    first commit

---
+++ LoginControlador.java
@@ -0,0 +1,28 @@
+package fxml;
+
+import org.controlsfx.control.Notifications;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginControlador {
+	
+	@FXML
+	private TextField campoEmail;
+	
+	@FXML
+	private PasswordField campoSenha;
+	
+	public void entrar() {
+		boolean emailValido = campoEmail.getText().equals("paulo@gmail.com");
+		boolean senhaValida = campoSenha.getText().equals("12345678");
+		
+		if(emailValido && senhaValida) {
+			Notifications.create().title("Login FXML").text("Login efetuado com sucesso!").showInformation();
+		} else {
+			Notifications.create().title("Login FXML").text("Usuário e Senha inválidos!").showError();
+		}
+	}
+
+}
